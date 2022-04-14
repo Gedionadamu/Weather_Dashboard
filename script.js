f1f21523e66d2f97b816c8a27d77c89b0a2d1cb8
Gedion Adamu <gedionadamu@gmail.com>
2022-04-14 00:37:52 -0400

    getting all data from the API

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-var APIkey = "5653051af8595ec21720cf5c767ab487 ";
+var APIkey = "5653051af8595ec21720cf5c767ab487";
+// var APIkey2 = "6ecdc1928c85022b921384c321b71183";
 var UserInput = $("input").val();
 var currentcity = "PlaceHolder";
-var today = moment();
-$("#currentDay").text( currentcity +" (" + today.format("M,D,YYYY")+")");
-
-
-
+var searchHistory = [];
 var searchBtn = $(".search");
+var days = 5
 
+
+
+
+function saveHistory(){
+    searchHistory.push(UserInput)
+    localStorage.setItem("savedHistory", JSON.stringify(searchHistory));
+    console.log(searchHistory);
+}
+function renderHistory(){
+
+}
 
 searchBtn.on("click", function(){
+    
     UserInput = $("input").val();
     var today = moment();
-    $("#currentDay").text( UserInput +" (" + today.format("M,D,YYYY")+")");
+    var lat = [];
+    var lon = [];
+    $("#currentDay").text( UserInput +" (" + today.format("M/D/YYYY")+")");
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${UserInput}&appid=${APIkey}&units=imperial`)
     .then(function(response){
     return response.json();
     })
     .then(function(data){
-    for (var i= 0; i < data.length; i++){
-        console.log(data[i].temp);
-        console.log(data[i].speed);
-        console.log(data[i].humidity);
-        console.log(data[i].uvi);    
-    }
-})
-    console.log(UserInput);
+        console.log(data)
+        console.log(data.main.temp);
+        console.log(data.wind.speed);
+        console.log(data.main.humidity);
+        console.log(data.coord.lat);    
+        console.log(data.coord.lon);
+        lat.push(data.coord.lat);
+        lon.push(data.coord.lon); 
+       
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${APIkey}&units=imperial`)
+        .then(function(response1){
+            return response1.json();
+        })
+        .then(function(data1){
+            console.log(data1.daily);
+            for(var i=0; i < 5; i++){
+                    console.log(data1.daily[i].temp.day)
+                    console.log(data1.daily[i].wind_speed)
+                    console.log(data1.daily[i].humidity)
+                    console.log(data1.daily[i].uvi)
+                }
+           
+        })      
+    })
+    
+    saveHistory();
 });
 
 
