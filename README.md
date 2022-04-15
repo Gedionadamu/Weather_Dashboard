3699685d68b6364370467ff60ccee50b449b3ce6
Gedion Adamu <gedionadamu@gmail.com>
2022-04-15 00:10:25 -0400

    added icons to the cards search history now works to update results.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Weather_Dashboard
+
+## User Story
+ 
+ As a traveler, 
+ The client wants to see the weather outlook for multiple cities
+ so that they can plan a trip accordingly.
+
+ ### Acceptance Criteria
+
+ ```
+
+Can search for a city with input feild in the dashboard.
+search history of user is saved for next time. 
+user can view current weather conditions for that searched city and presented with.
+with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index.
+The UV index indicates whether the conditions are favorable, moderate, or severe.
+User is also have access to future weather conditions for that city upto 5 days.
+The 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, UV index and the humidity
+user can click on a city in the search history be presented with current and future conditions for that city
+```
+
+### ScreenShot 
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Paris.](./screenshot%202.jpg)
+![landing page.](./screenshot%201.jpg)
+
+### GitHub Deployed site
--- script.js
+++ script.js
@@ -1,108 +1,135 @@
+// variable designation for api keyfor open weather
 var APIkey = "5653051af8595ec21720cf5c767ab487";
-// var APIkey2 = "6ecdc1928c85022b921384c321b71183";
+// variable for userinput
 var UserInput = $("input").val();
-var currentcity = "PlaceHolder";
+//empty array to push local storage 
 var searchHistory = [];
+// search button selection
 var searchBtn = $(".search");
+// card element selection
 var cardEl = $(".card");
+
 var days = 5;
+var today = moment();
 
 
-
- function init(){
-     var savedsearch = JSON.parse(localStorage.getItem("savedHistory"));
-     if (savedsearch !== null){
-         savedsearch = searchHistory;
-     }
-     renderHistory();
- }
-function saveHistory(){
+// function that renders localstorage when page loads
+function init() {
+    var savedsearch = JSON.parse(localStorage.getItem("savedHistory"));
+    if (savedsearch !== null) {
+        savedsearch = searchHistory;
+    }
+    renderHistory();
+}
+// function that save seach history to local storage
+function saveHistory() {
     searchHistory.push(UserInput)
     localStorage.setItem("savedHistory", JSON.stringify(searchHistory));
     // console.log(searchHistory);
 }
-function renderHistory(){
+// places search history in local storage array into buttons
+function renderHistory() {
     var localsearch = JSON.parse(localStorage.getItem("savedHistory"));
-    console.log(localsearch);
-       var historyEl = $('.history')
-       if (localsearch == null){
-           return
-       }
-       else{
-    for(var i=0; i < localsearch.length; i++) {
-        var search = $('<button>');
-        search.text(localsearch[i]);
-        search.addClass('btn btn-secondary historybtn ms-1 mt-1');
-        historyEl.append(search);
 
+
+    if (localsearch == null) {
+        return
     }
-}
+    else {
+        for (var i = 0; i < localsearch.length; i++) {
+            createhistorybtn(localsearch[i]);
+
+        }
+    }
 
 
 }
-//  var historybutton = $(".historybtn");
-//  console.log(historybutton);
-//  historybutton.on("click", function(){
-//      var txt = $(this).val();
-//      console.log(txt)
+// creates and displays history buttons on html
+function createhistorybtn(cityname) {
+    var historyEl = $('.history')
+    var search = $('<button>');
+    search.text(cityname);
+    search.addClass('btn btn-secondary historybtn ms-1 mt-1');
+    search.on("click", function () {
+        console.log(search.text())
+        fetchdata(search.text());
+    });
+    historyEl.append(search);
 
-//  })
-//  console.log
-
-
-searchBtn.on("click", function(){
-    
-    UserInput = $("input").val();
-    var today = moment();
+}
+// gets data from openweathermap.org
+function fetchdata(cityname) {
     var lat = [];
     var lon = [];
-    $("#currentDay").text( UserInput +" (" + today.format("M/D/YYYY")+")");
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${UserInput}&appid=${APIkey}&units=imperial`)
-    .then(function(response){
-    return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        $("#currenttemp").text("Current Temprature : " + data.main.temp + "°F");
-        $("#currentwind").text("Current Wind : " + data.wind.speed + "MPH");
-        $("#currenthumidity").text("Current Humidity : " + data.main.humidity + "%");
-        console.log(data.coord.lat);    
-        console.log(data.coord.lon);
-        lat.push(data.coord.lat);
-        lon.push(data.coord.lon);
-        
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${APIkey}`)
-        .then(function(response2){
-            return response2.json();
+    // fetch api data 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=imperial`)
+        .then(function (response) {
+            return response.json();
         })
-        .then(function(data2){
-            $("#currentuv").text("Current UVI : " + data2.current.uvi)
-        })
-       
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${APIkey}&units=imperial`)
-        .then(function(response1){
-            return response1.json();
-        })
-        .then(function(data1){
-            console.log(data1.daily);
-            for(var i=0; i < 5; i++){
-                
-                    $("#day"+[i]).text(moment().add(i+1, 'days').format("M/D/YYYY"));
-                    $("#temp"+[i]).text("Temprature : " + data1.daily[i].temp.day + "°F" );
-                    $("#wind"+[i]).text( "Wind : " + data1.daily[i].wind_speed  + "MPH");
-                    $("#humidity"+[i]).text("Humidity : " + data1.daily[i].humidity + "%" );
-                    $("#uv"+[i]).text( "UVI : " + data1.daily[i].uvi);
-                }
-           
-        })      
-    })
-    saveHistory();
-    renderHistory();
-    
-    
-});
+        .then(function (data) {
+            // console.log(data)
+            // using api data to update html elements
+            $("#currenticon").attr("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+            $("#currenttemp").text("Current Temprature : " + data.main.temp + "°F");
+            $("#currentwind").text("Current Wind : " + data.wind.speed + "MPH");
+            $("#currenthumidity").text("Current Humidity : " + data.main.humidity + "%");
+            // console.log(data.coord.lat);
+            // console.log(data.coord.lon);
+            lat.push(data.coord.lat);
+            lon.push(data.coord.lon);
+            // fetch api data to get UVI data
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${APIkey}`)
+                .then(function (response2) {
+                    return response2.json();
+                })
+                .then(function (data2) {
+                    // using api data to update uvi index on html
+                    $("#currentuv").text("Current UVI : " + data2.current.uvi)
+                    // conditional statement to indicate conditions for uv index is favorable, moderate or severe
+                    if(data2.current.uvi <= 2){
+                        $("#currentuv").attr("class","bg-success")
+                    }
+                    else if(2 < data2.current.uvi <=7){
+                        $("#currentuv").attr("class","bg-warning")
+                    }
+                    else{
+                        $("#currentuv").attr("class","bg-danger")
 
+                    }
+                })
+                // fetch data to get the 5 day forecast
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${APIkey}&units=imperial`)
+                .then(function (response1) {
+                    return response1.json();
+                })
+                .then(function (data1) {
+                    console.log(data1.daily);
+                    // create a for loop to iterate through the card and put relevant date in the cards
+                    for (var i = 0; i < 5; i++) {
+                        $("#day" + [i]).text(moment().add(i + 1, 'days').format("M/D/YYYY"));
+                        $("#img"+[i]).attr("src", `http://openweathermap.org/img/wn/${data1.daily[i].weather[0].icon}@2x.png`)
+                        $("#temp" + [i]).text("Temprature : " + data1.daily[i].temp.day + "°F");
+                        $("#wind" + [i]).text("Wind : " + data1.daily[i].wind_speed + "MPH");
+                        $("#humidity" + [i]).text("Humidity : " + data1.daily[i].humidity + "%");
+                        $("#uv" + [i]).text("UVI : " + data1.daily[i].uvi);
+                    }
+
+                })
+        })
+}
+// an event lister on the search button to call multiple function the get data, modify data and append to html
+searchBtn.on("click", function () {
+    UserInput = $("input").val();
+    $("#currentDay").text(UserInput + " (" + today.format("M/D/YYYY") + ")");
+    fetchdata(UserInput);
+    createhistorybtn(UserInput);
+    saveHistory();
+
+
+
+});
+// calls a function that  renders localstorage when page loads
 init();
 
 
-    
+
